0eab4985641d8a11b0f57fcead8a1dd34efd4060
Nicolas Venegas <37488794+nicvenegas@users.noreply.github.com>
2022-08-02 17:54:00 +1000

    xbar: Simplify plugin by having a separate magic-keyboard.kbd

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
+CONFIGS := apple.kbd magic-keyboard.kbd
 PLUGIN = ${HOME}/Library/Application\ Support/xbar/plugins/KMonad.1m.sh
+
+all: $(CONFIGS) $(PLUGIN)
+
+$(CONFIGS): apple.kbd.m4
+
+apple.kbd:
+	m4 -D __NAME__='Apple Internal Keyboard / Trackpad' "$<" > "$@"
+
+magic-keyboard.kbd:
+	m4 -D __NAME__='Magic Keyboard' "$<" > "$@"
 
 $(PLUGIN): xbar/KMonad.sh
 	cp "$<" "$@"
 	chmod u+x "$@"
 
 clean:
+	rm $(CONFIGS)
 	rm $(PLUGIN)
--- xbar/KMonad.sh
+++ xbar/KMonad.sh
@@ -4,27 +4,27 @@ CONFIGDIR="${HOME}/Code/kmonad-config"
 PLUGINSDIR="${HOME}/Library/Application Support/xbar/plugins"
 SLEEP=1
 
-script="${PLUGINSDIR}/$(basename $0)"
-opts="shell='${script}' | terminal=true | refresh=true"
+opts="shell='${PLUGINSDIR}/$(basename $0)' | terminal=true | refresh=true"
 
 if [[ -z "$1" ]]; then
   sleep "${SLEEP}"
   echo "⌃⌥⌘⇧"
   echo "---"
 
-  runinfo=$(pgrep -lf 'sudo kmonad' | cut -d ' ' -f 6)
+  runinfo=$(pgrep -lf 'sudo kmonad')
   case $runinfo in
-    *apple.kbd) echo "Apple Internal Keyboard / Trackpad ✓" ;;
-    *)          echo "Apple Internal Keyboard / Trackpad | ${opts} | param1=apple" ;;
+    *apple*) echo "Apple Internal Keyboard / Trackpad ✓" ;;
+    *)       echo "Apple Internal Keyboard / Trackpad | ${opts} | param1=apple" ;;
   esac
   case $runinfo in
-    *kinesis.kbd) echo "Kinesis Freestyle Pro ✓" ;;
-    *)            echo "Kinesis Freestyle Pro | ${opts} | param1=kinesis" ;;
+    *kinesis*) echo "Kinesis Freestyle Pro ✓" ;;
+    *)         echo "Kinesis Freestyle Pro | ${opts} | param1=kinesis" ;;
   esac
   case $runinfo in
-    --input) echo "Magic Keyboard ✓" ;;
-    *)       echo "Magic Keyboard | ${opts} | param1=magic-keyboard" ;;
+    *magic-keyboard*) echo "Magic Keyboard ✓" ;;
+    *)                echo "Magic Keyboard | ${opts} | param1=magic-keyboard" ;;
   esac
+
   echo "---"
   if [[ -z "${runinfo}" ]]; then
     echo "Kill"
@@ -35,12 +35,8 @@ if [[ -z "$1" ]]; then
   exit 0
 fi
 
-
 sudo pkill kmonad
-sleep "${SLEEP}"
-
-case $1 in
-  apple) sudo kmonad --start-delay 0 "${CONFIGDIR}/apple.kbd" & ;;
-  kinesis) sudo kmonad --start-delay 0 "${CONFIGDIR}/kinesis.kbd" & ;;
-  magic-keyboard) sudo kmonad --start-delay 0 --input 'iokit-name "Magic Keyboard"' "${CONFIGDIR}/apple.kbd" & ;;
-esac
+if [[ "$1" != "kill" ]]; then
+  sleep "${SLEEP}"
+  sudo kmonad --start-delay 0 "${CONFIGDIR}/$1.kbd" &
+fi
